a9e4571beb23839e5c804221370aea9409617b54
Xianzhi(Fred) Hua <40416564+FredXHua@users.noreply.github.com>
2020-05-19 01:03:45 -0700

    loading screen made, not implemented

---
+++ lib/loading.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: SpinKitFoldingCube(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      ),
+    );
+  }
+}
