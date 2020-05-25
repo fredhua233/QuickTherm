01827500707dbce977bcbb9fa2668b9c584fc7d8
Xianzhi(Fred) Hua <40416564+FredXHua@users.noreply.github.com>
2020-05-25 15:50:39 -0700

    added user info class

--- lib/Pages/SetUpInfoPage.dart
+++ lib/Pages/SetUpInfoPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Utils/UserInfo.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class setUpInfoPage extends StatefulWidget {
@@ -7,19 +8,8 @@ class setUpInfoPage extends StatefulWidget {
 }
 
 class _setUpInfoPageState extends State<setUpInfoPage> {
-  String _firstName;
-  String _lastName;
-  String _address;
-  String _phoneNumber;
-  String _managerName;
-  String _sex;
-  String _healthHistory;
-  String _healthStatus;
-
-  ///combine
-  String _condition;
-  String _DOB;
-  String _age;
+  final _formKey = new GlobalKey<FormState>();
+  final _user = UserInfo();
 
   @override
   Widget build(BuildContext context) {
@@ -28,13 +18,62 @@ class _setUpInfoPageState extends State<setUpInfoPage> {
           title: Text('Fill out your information'),
         ),
         body: Container(
-          margin: EdgeInsets.all(24),
-          child: Form(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [],
-            ),
-          ),
-        ));
+            padding: EdgeInsets.all(24),
+            child: Builder(
+              builder: (context) => Form(
+                key: _formKey,
+                autovalidate: true,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    new ListView(
+                      padding: EdgeInsets.all(20),
+                      children: <Widget> [
+                        TextFormField(
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.calendar_today),
+                              hintText: 'Enter your first and last name (Ex. John Doe)',
+                              labelText: 'Name'
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty){
+                              return 'Please enter your name';
+                            }
+                            return null;
+                          },
+                          onSaved: (val) => setState(() => _user.Name = val),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget> [
+                            RadioListTile(
+                              title: const Text('Male'),
+                              value: 0,
+                              onChanged: (value) => {
+                                setState(() {
+                                  _user.sex = value;
+                                })
+                              },
+                            ),
+                            RadioListTile(
+                              title: const Text('Female'),
+                              value: 1,
+                              onChanged: (value) => {
+                                setState(() {
+                                  _user.sex = value;
+                                })
+                              },
+                            )
+                          ],
+                        )
+
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+        )
+    );
   }
 }
---
+++ lib/Utils/UserInfo.dart
@@ -0,0 +1,17 @@
+//  import 'package:flutter/material.dart';
+
+class UserInfo {
+  String Name;
+  String address;
+  String phoneNumber;
+  String managerName;
+  String sex;
+  String healthHistory;
+  String healthStatus;
+  String condition;
+  String DOB;
+  String age;
+  bool reminderDaily;
+//  TimeOfDay remindTime;
+///where to put persistence? here or Utils?
+}
